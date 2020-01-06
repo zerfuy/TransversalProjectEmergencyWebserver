9fdeddc05f57d735a7d7015b957e4dfaa2ce9f1a
Gabriel <gab.bailly@gmail.com>
2020-01-06 15:20:28 +0100

    Correctif, fonctionnel à 100%. Pas oublier de brancher les deux cartes !

--- app.py
+++ app.py
@@ -49,12 +49,9 @@ assets.register('css_all', css)
 
 mysql = MySQL(app)
 
-
 # potential improvments : 
 # async calls via multiple connections
-# functions
 @app.route('/')
-
 def index():
 
     # read serial port to get fires
@@ -65,23 +62,21 @@ def index():
 
     a = ""
 
-    # ser = serial.Serial(
-    #     port=SERIALPORT,
-    #     baudrate=115200
-    # )
+    ser = serial.Serial(
+        port=SERIALPORT,
+        baudrate=115200
+    )
 
     try:
         pass
-        # a = ser.readline()
-        # ser.close()
+        a = ser.readline()
+        ser.close()
     except serial.SerialException:
         print("Serial {} port not available".format(SERIALPORT))
 
-    a = "59,1;58,1;57,0;56,0;55,0;54,0;53,0;52,0;51,0;50,0;49,0;48,1;47,0;46,0;45,0;44,0;43,0;42,0;41,0;40,0;39,0;38,0;37,0;36,0;35,0;34,0;33,0;32,0;31,0;30,0;29,0;28,0;27,0;26,0;25,0;24,0;23,0;22,0;21,0;20,0;19,0;18,0;17,0;16,0;15,0;14,0;13,0;12,0;11,0;10,0;9,0;8,0;7,0;6,0;5,0;4,0;3,0;2,0;1,0"
-
     tab = []
     finalfires = []
-    tab = a.split(";")
+    tab = str(a, 'utf-8').split(";")
     for fire in tab:
         finalfires.append(fire.split(","))
 
@@ -101,19 +96,20 @@ def index():
     fire_engine_table["fire_engine_table_name"] = "fire_engine"
 
     intervention_table = {}
+    intervention_table["intervention_id"] = "intervention.id"
     intervention_table["intervention_id_fire_engine"] = "id_fire_engine"
     intervention_table["intervention_id_fire"] = "id_fire"
     intervention_table["intervention_table_name"] = "intervention"
     intervention_table["intervention_route"] = "route"
 
     fire_table = {}
-    fire_table["fire_id"] = "id"
-    fire_table["fire_id_real_pos"] = "id_real_pos"
+    fire_table["fire_id"] = "fire.id"
+    fire_table["fire_id_real_pos"] = "fire.id_real_pos"
     fire_table["fire_intensity"] = "intensity"
     fire_table["fire_table_name"] = "fire"
 
     real_pos_table = {}
-    real_pos_table["real_pos_id"] = "id"
+    real_pos_table["real_pos_id"] = "real_pos.id"
     real_pos_table["real_pos_real_x"] = "real_x"
     real_pos_table["real_pos_real_y"] = "real_y"
     real_pos_table["real_pos_name"] = "real_pos"
@@ -131,63 +127,88 @@ def index():
         )
     conn.commit()
 
-    # get fire_engines positions in fire_engines
-    cur.execute("select {0}, {1}, {2}, {3} from {4}".format(
+    # get fire_engines
+    cur.execute("select {0}, {1}, {2} from {3} where {4} in (select {5} from {6})".format(
+        fire_engine_table["fire_engine_x_pos"],
+        fire_engine_table["fire_engine_y_pos"],
         fire_engine_table["fire_engine_id"],
-        fire_engine_table["fire_engine_id_station"],
-        fire_engine_table["fire_engine_x_pos"],
-        fire_engine_table["fire_engine_y_pos"],
-        fire_engine_table["fire_engine_table_name"])
-    )
-    fire_engines = []
-    row = cur.fetchone()
-    while row is not None:
-        fire_engines.append(list(row[2:]))
-        row = cur.fetchone()
-
-    # get intervention relations
-    cur.execute("select {0}, {1} from {2} where {3} in (select {4} from {5})".format(
-        fire_engine_table["fire_engine_x_pos"],
-        fire_engine_table["fire_engine_y_pos"],
         fire_engine_table["fire_engine_table_name"],
         fire_engine_table["fire_engine_id"],
         intervention_table["intervention_id_fire_engine"],
         intervention_table["intervention_table_name"])
     )
-    fire_engines_pos = []
+    fire_engines = []
     row = cur.fetchone()
     while row is not None:
-        fire_engines_pos.append(list(row))
+        fire_engines.append(list(row))
         row = cur.fetchone()
 
-    # get active fire positions
-    cur.execute("select {0}, {1} from {2} where {3} in (select {4} from {5} where {6} in (select {7} from {8}))".format(
+    # get fire_engines positions
+    fire_engines_pos = []
+    for elem in fire_engines:
+        fire_engines_pos.append(elem[:-1])
+
+    # get active fires
+    cur.execute("select {0}, {1}, {2} from {3}, {4} where {5} = {6}".format(
         real_pos_table["real_pos_real_x"],
         real_pos_table["real_pos_real_y"],
-        real_pos_table["real_pos_name"],
-        real_pos_table["real_pos_id"],
-        fire_table["fire_id_real_pos"],
-        fire_table["fire_table_name"],
         fire_table["fire_id"],
-        intervention_table["intervention_id_fire"],
-        intervention_table["intervention_table_name"]))
+        fire_table["fire_table_name"],
+        real_pos_table["real_pos_name"],
+        fire_table["fire_id_real_pos"],
+        real_pos_table["real_pos_id"])
+    )
     fires = []
     row = cur.fetchone()
     while row is not None:
         fires.append(list(row))
         row = cur.fetchone()
 
-    # Establish itineraries from fire_engines to active fires
+    # get active fires positions
+    fire_pos = []
+    for elem in fires:
+        fire_pos.append(elem[:-1])
+
+    # get all interventions, so we can use fire/engine link
+    cur.execute("select {0}, {1}, {2} from {3}".format(
+        intervention_table["intervention_id_fire_engine"],
+        intervention_table["intervention_id_fire"],
+        intervention_table["intervention_id"],
+        intervention_table["intervention_table_name"]
+        ))
+    interventions_complex = []
+    row = cur.fetchone()
+    while row is not None:
+        interventions_complex.append(list(row))
+        row = cur.fetchone()
+    interventions = []
+    for elem in interventions_complex:
+        interventions.append(elem[:-1])
+
+    # Establish itineraries from fire_engines to active fires, according to the existing interventions
     fromTo = []
-    if len(fire_engines_pos) == len(fires):
-        for i in range(0, len(fire_engines_pos)):
-            fromTo.append([fire_engines_pos[i], fires[i]])
+    for intervention in interventions:
+        idEngine = intervention[0]
+        idFire = intervention[1]
+        fromEngine = []
+        toFire = []
+        for item in fire_engines:
+            if item[2] == idEngine:
+                fromEngine = item[:-1]
+        for item in fires:
+            if item[2] == idFire:
+                toFire = item[:-1]
+        fromTo.append([fromEngine, toFire])
+
+
+    # TODO
 
     # Parse fire_engines from 0 à x, donc on peut attribuer les chemins sans le meme ordre
     routingInfo = list()
     for ft in fromTo:
         link = "http://router.project-osrm.org/route/v1/driving/" + str(ft[0][1]) + "," + str(ft[0][0]) + ";" + str(ft[1][1]) + "," + str(ft[1][0]) + "?overview=full"
-        if "routes" in requests.get(link).json().keys():
+        if "routes" in requests.get(link).json():
+            # ctypes.windll.user32.MessageBoxW(0, "updating", "Your title", 1)
             res = requests.get(link).json()["routes"][0]["geometry"]
             routingInfo.append(res)
             # insert routes in db
@@ -217,4 +238,4 @@ def index():
             routingInfo.append(str(cur.fetchone()[0]))
 
     cur.close()
-    return render_template('index.html', fire_engines=fire_engines, routingInfo=routingInfo, fires=fires)
+    return render_template('index.html', fire_engines=fire_engines_pos, routingInfo=routingInfo, fires=fire_pos)
